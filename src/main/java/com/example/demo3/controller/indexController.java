8fedbc00a12b6ca9573e73b8d5664d9c0f5d47c1
cwh <lion946@naver.com>
2021-12-21 15:40:32 +0900

    Add IndexController

---
+++ src/main/java/com/example/demo3/controller/indexController.java
@@ -0,0 +1,11 @@
+package com.example.demo3.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class indexController {
+    @GetMapping("/")
+    public String hello(){
+        return "Hello!!";
+
+    }
+}
