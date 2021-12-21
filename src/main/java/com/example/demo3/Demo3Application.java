195da95db3ac629d60ef60e77f4af927dcd8199e
cwh <lion946@naver.com>
2021-12-21 16:29:37 +0900

    modify Project

--- src/main/java/com/example/demo3/Demo3Application.java
+++ src/main/java/com/example/demo3/Demo3Application.java
@@ -2,13 +2,19 @@ package com.example.demo3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Demo3Application {
+public class DemoApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
-        SpringApplication.run(Demo3Application.class, args);
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(DemoApplication.class);
     }
 
 }
